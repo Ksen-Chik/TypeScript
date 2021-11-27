48b7b821ae471b64c6733fe4a6c26231c18dd9ef
KseniyaNakaryakova <narciss-lotos@yandex.ru>
2021-11-27 14:59:47 +0400

    ДЗ к уроку 3

---
+++ src/todo.ts
@@ -0,0 +1,17 @@
+export class Todo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+export async function getTodosByCount(count: number): Promise<Todo[]> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_start=0&_limit=${count}`);
+  const json = await response.json();
+
+  json.forEach(todo => Object.setPrototypeOf(todo, Todo.prototype));
+
+  console.log(json);
+
+  return json;
+}
